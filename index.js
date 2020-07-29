68b575661693e6147b08a20f8ea09f478a6c7079
lesleyc@bu.edu <lesleyc@bu.edu>
2020-07-29 16:53:17 +0800

    Added notes

--- index.js
+++ index.js
@@ -4,6 +4,25 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 const db = require("./db");
+const bodyParser = require("body-parser");
+
+const hbs = require("express-handlebars");
+const { response } = require("express");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+// Configure for HBS
+app.engine(
+  "hbs",
+  hbs({
+    extname: ".hbs",
+    defaultLayout: "main",
+    layoutsDir: __dirname + "/views/layouts",
+  })
+);
+app.set("view engine", "hbs");
+
 const create_user_table = `
 CREATE TABLE users (
     username VARCHAR(255) primary key,  
@@ -48,28 +67,39 @@ function execute(query) {
   });
 }
 // execute(create_user_table);
-execute(get_notes);
+//execute(get_notes);
 
 function addNote(username, noteid, content) {
   let string = `INSERT INTO notes (username, noteId, content) VALUES ('${username}', '${noteid}', '${content}');`;
   return string;
 }
 
-execute(get_notes);
-
-// #TODO: CONTINUE TO GO THROUGH THE TUTORIAL
-// #TODO: CREATE USER TABLE
-
-// #TODO: CREATE NOTES TABLE
-// #TODO: ADD USER
-// #TODO: ADD NOTE
-// #TODO: GET NOTE
-// #TODO: GET USER
-// #TODO: DELETE NOTE
-// #TODO: DELETE USER
-// #TODO: EDIT NOTE
-// #TODO: EDIT USER
+let allnotes = execute(get_notes);
 
 // #TODO: SET UP ROUTES WITH THE QUERY
+
+// #TODO: collect data from frontend
+// #TODO: and then pass the data into the function and then send to backend
+
+app.get("/", (req, res) => {
+  return res.redirect("/form_get");
+});
+app.get("/form_get", (req, res) => {
+  return res.render("form_get");
+});
+
+app.get("/submit_get", (req, res) => {
+  return res.send(req.query);
+});
+
+app.get("/form_post", (req, res) => {
+  return res.render("form_post");
+});
+
+app.post("/submit_post", (req, res) => {
+  return res.send(req.body);
+});
+app.listen(3000, () => {
+  console.log("app listening on 3000");
+});
 // #TODO: SET UP WITH POSTMAN
-// #TODO:
